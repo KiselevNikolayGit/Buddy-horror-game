53eb5bc0c2762803e5e38e4c2a99d63ff58609a4
KiselevNikolayGit <neondoctors@gmail.com>
2017-07-13 19:49:10 +0300

    ver 0. 0. 0. 2

--- main.lua
+++ main.lua
@@ -14,19 +14,46 @@ function fillt()
 	fillter:setFilter("nearest")
 end
 
+function butcoll(x, y, text, fun)
+	if x > text.x and y > text.y and x < text.x + text.w and y < text.y + text.h then
+		fun()
+	end
+end
+
 function love.load()
 	do -- Window
-		love.window.setFullscreen(true)
-		local w, h = love.window.getMode()
-		size = h
-		side = (w - size) / 2
+		-- love.window.setFullscreen(true)
+		local wid, hid = love.window.getMode()
+		size = hid
+		side = (wid - size) / 2
 	end
-	do -- Font
+	do -- Draw
 		font = love.graphics.newFont("Press_Start_2P/PressStart2P-Regular.ttf", 50)
 		font:setFilter("nearest")
+		do -- Title
+			title = {}
+			title.t = love.graphics.newText(font, "Buddy Horror Story")
+			title.rw = ((size - 20) / title.t:getWidth())
+			title.rh = 1
+			title.x = 10
+			title.y = 20
+			title.w = title.t:getWidth() * title.rw
+			title.h = title.t:getHeight() * title.rh
+		end
+		do -- Start
+			start = {}
+			start.t = love.graphics.newText(font, "Start over")
+			start.rw = ((size - 200) / start.t:getWidth())
+			start.rh = 0.5
+			start.x = 100
+			start.y = 30 + title.h
+			start.w = start.t:getWidth() * start.rw
+			start.h = start.t:getHeight() * start.rh
+		end
+		love.graphics.setBackgroundColor(30, 25, 33)
 	end
 	do -- Sides
-		backimg = love.graphics.newImage("back.bmp")
+		backimg = love.graphics.newImage("iso/back.bmp")
 		backimg:setWrap("repeat")
 		backimg:setFilter("nearest")
 		local w, h = love.window.getMode()
@@ -84,7 +111,7 @@ function love.load()
 	end
 	do -- Audio
 		local a = {}
-		a.main = love.audio.newSource("reer.ogg", "static")
+		a.main = love.audio.newSource("sou/run.ogg", "static")
 		love.audio.play(a.main)
 	end
 end
@@ -118,6 +145,11 @@ function love.keyreleased(key)
 end
 
 function love.mousepressed(x, y, button, isTouch)
+	x = x - side
+	do -- Buttons
+		butcoll(x, y, title, function() print("roar") end)
+		butcoll(x, y, start, function() print("start") end)
+	end
 end
 
 function love.mousereleased(x, y, button, isTouch)
@@ -128,16 +160,20 @@ end
 
 function love.draw()
 	do -- Sides
+		love.graphics.setColor(255, 255, 255)		
 		love.graphics.translate(side, 0)
 		love.graphics.draw(mesh, -side, 0)
 		love.graphics.draw(mesh, size, 0)
 	end
 	do -- Menu
+		love.graphics.setColor(109, 84, 103)
 		love.graphics.setFont(font)
-		love.graphics.print(tostring(love.timer.getFPS()), -side + 10, 10, -0.1, 0.2, 0.2)
-		love.graphics.print("fdsgdfgsdfgsdfgs", 10, 10, 0, 0.5, 0.5)
+		love.graphics.print(tostring(love.timer.getFPS()), -side + 10, 10, -0.06, 0.2, 0.2)
+		love.graphics.draw(title.t, title.x, title.y, 0, title.rw, title.rh)
+		love.graphics.draw(start.t, start.x, start.y, 0, start.rw, start.rh)
 	end
 	do -- Fillter
+		love.graphics.setColor(255 / 2, 255 / 2, 255 / 2)		
 		love.graphics.draw(fillter, -side, 0, 0, 5, 5)
 	end
 end
