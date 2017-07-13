1a3ff0f719374508ca8ec4d4554afae20ed5abed
KiselevNikolayGit <neondoctors@gmail.com>
2017-07-13 11:30:11 +0300

    ver 0. 0. 0. 1

---
+++ main.lua
@@ -0,0 +1,26 @@
+function love.load()
+end
+
+function love.wheelmoved(x, y)
+end
+
+function love.keypressed(key, scancode, isrepeat)
+end
+
+function love.keyreleased(key)
+end
+
+function love.mousepressed(x, y, button, isTouch)
+end
+
+function love.mousereleased(x, y, button, isTouch)
+end
+
+function love.textinput(text)
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+end
